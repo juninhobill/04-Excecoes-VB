7e59f8e49fd4d5319330f5eb1820bc2f556321f8
juninhobill <billv6@gmail.com>
2021-10-20 07:14:46 -0300

    Add project files.

---
+++ Excecoes/Frm_Principal.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Principal))
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArquivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalvarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuscarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RecortarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OnlineToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SobreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ArquivoTextoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ArquivoPlanilhaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(12, 88)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Principal.TabIndex = 0
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArquivoToolStripMenuItem, Me.EditarToolStripMenuItem, Me.AjudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(534, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArquivoToolStripMenuItem
+        '
+        Me.ArquivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoToolStripMenuItem, Me.AbrirToolStripMenuItem, Me.SalvarToolStripMenuItem, Me.SairToolStripMenuItem})
+        Me.ArquivoToolStripMenuItem.Name = "ArquivoToolStripMenuItem"
+        Me.ArquivoToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ArquivoToolStripMenuItem.Text = "Arquivo"
+        '
+        'EditarToolStripMenuItem
+        '
+        Me.EditarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BuscarToolStripMenuItem, Me.CopairToolStripMenuItem, Me.RecortarToolStripMenuItem, Me.ColarToolStripMenuItem})
+        Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
+        Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.EditarToolStripMenuItem.Text = "Editar"
+        '
+        'AjudaToolStripMenuItem
+        '
+        Me.AjudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OnlineToolStripMenuItem, Me.SobreToolStripMenuItem})
+        Me.AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
+        Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.AjudaToolStripMenuItem.Text = "Ajuda"
+        '
+        'NovoToolStripMenuItem
+        '
+        Me.NovoToolStripMenuItem.Name = "NovoToolStripMenuItem"
+        Me.NovoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NovoToolStripMenuItem.Text = "Novo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArquivoTextoToolStripMenuItem, Me.ArquivoPlanilhaToolStripMenuItem})
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'SalvarToolStripMenuItem
+        '
+        Me.SalvarToolStripMenuItem.Name = "SalvarToolStripMenuItem"
+        Me.SalvarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SalvarToolStripMenuItem.Text = "Salvar"
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SairToolStripMenuItem.Text = "Sair"
+        '
+        'BuscarToolStripMenuItem
+        '
+        Me.BuscarToolStripMenuItem.Name = "BuscarToolStripMenuItem"
+        Me.BuscarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.BuscarToolStripMenuItem.Text = "Buscar"
+        '
+        'CopairToolStripMenuItem
+        '
+        Me.CopairToolStripMenuItem.Image = CType(resources.GetObject("CopairToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CopairToolStripMenuItem.Name = "CopairToolStripMenuItem"
+        Me.CopairToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CopairToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CopairToolStripMenuItem.Text = "Copiar"
+        '
+        'RecortarToolStripMenuItem
+        '
+        Me.RecortarToolStripMenuItem.Name = "RecortarToolStripMenuItem"
+        Me.RecortarToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.RecortarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RecortarToolStripMenuItem.Text = "Recortar"
+        '
+        'ColarToolStripMenuItem
+        '
+        Me.ColarToolStripMenuItem.Name = "ColarToolStripMenuItem"
+        Me.ColarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ColarToolStripMenuItem.Text = "Colar"
+        '
+        'OnlineToolStripMenuItem
+        '
+        Me.OnlineToolStripMenuItem.Name = "OnlineToolStripMenuItem"
+        Me.OnlineToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OnlineToolStripMenuItem.Text = "Online"
+        '
+        'SobreToolStripMenuItem
+        '
+        Me.SobreToolStripMenuItem.Name = "SobreToolStripMenuItem"
+        Me.SobreToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SobreToolStripMenuItem.Text = "Sobre"
+        '
+        'ArquivoTextoToolStripMenuItem
+        '
+        Me.ArquivoTextoToolStripMenuItem.Name = "ArquivoTextoToolStripMenuItem"
+        Me.ArquivoTextoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ArquivoTextoToolStripMenuItem.Text = "Arquivo Texto"
+        '
+        'ArquivoPlanilhaToolStripMenuItem
+        '
+        Me.ArquivoPlanilhaToolStripMenuItem.Name = "ArquivoPlanilhaToolStripMenuItem"
+        Me.ArquivoPlanilhaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ArquivoPlanilhaToolStripMenuItem.Text = "Arquivo Planilha"
+        '
+        'Frm_Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 312)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Frm_Principal"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArquivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ArquivoTextoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ArquivoPlanilhaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalvarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuscarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RecortarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OnlineToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SobreToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Excecoes/Frm_Principal.vb
@@ -0,0 +1,23 @@
+﻿Public Class Frm_Principal
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Me.Text = "Principal"
+        Lbl_Principal.Text = "Principal"
+
+    End Sub
+
+    Private Sub Lbl_Principal_Click(sender As Object, e As EventArgs) Handles Lbl_Principal.Click
+
+    End Sub
+
+    Private Sub CopairToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopairToolStripMenuItem.Click
+
+        MsgBox("Escolhi a opção copiar!")
+
+    End Sub
+End Class
